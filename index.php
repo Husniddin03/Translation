9ec80162d0fba2dfc57ac2caa0ce42b1459b4f3d
Husniddin03 <husniddin13124041@gmail.com>
2024-05-22 20:44:06 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+include 'translation.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Translator</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <form method="post" action="" class="row">
+            <div>
+                <h3 class="mt-5">Translator</h3>
+                <button name="submit" type="submit" class="btn btn-info">Translation</button>
+            </div>
+            <div class="col-xxl col-12-lg mt-3">
+                <select name="from" class="mb-3 form-select" aria-label="Default select example">
+                    <option selected><?=isset($from)?$languages[$from]:"From which language"?></option>
+                    <?php foreach($languages as $key=>$item){
+                        echo '<option value="'.$key.'">'.$item.'</option>';
+                    }?>
+                </select>
+                <div class="form-floating">
+                    <textarea name="text" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 200px"><?=isset($text)?$text:""?></textarea>
+                    <label for="floatingTextarea2">Text</label>
+                </div>
+            </div>
+            <div class="col-xxl col-12-lg mt-3">
+                <select name="to" class="mb-3 form-select" aria-label="Default select example">
+                    <option selected><?=isset($to)?$languages[$to]:"Which language"?></option>
+                    <?php foreach($languages as $key=>$item){
+                        echo '<option value="'.$key.'">'.$item.'</option>';
+                    }?>
+                </select>
+                <div class="form-floating mb-3">
+                    <textarea name="translation" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2Disabled" style="height: 200px" disabled><?=isset($translation)?$translation:""?></textarea>
+                    <label for="floatingTextarea2Disabled">Text</label>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ translation.php
@@ -0,0 +1,69 @@
+<?php
+
+
+if (isset($_POST['submit'])) {
+    $text = $_POST['text'];
+    $from = $_POST['from'];
+    $to = $_POST['to'];
+    $url = "https://api.mymemory.translated.net/get?q=" . urlencode($text) . "&langpair={$from}|{$to}";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $responseArray = json_decode($response, true);
+
+    if (isset($responseArray['responseData']['translatedText'])) {
+        $translation = $responseArray['responseData']['translatedText'];
+    } else {
+        $translation = "Translation not found.";
+    }
+}
+
+$languages = [
+    'uz' => 'Uzbek',
+    'en' => 'English',
+    'ru' => 'Russian',
+    'fr' => 'French',
+    'de' => 'German',
+    'es' => 'Spanish',
+    'it' => 'Italian',
+    'zh' => 'Chinese',
+    'ja' => 'Japanese',
+    'ko' => 'Korean',
+    'ar' => 'Arabic',
+    'hi' => 'Hindi',
+    'pt' => 'Portuguese',
+    'bn' => 'Bengali',
+    'pa' => 'Punjabi',
+    'jv' => 'Javanese',
+    'ms' => 'Malay',
+    'id' => 'Indonesian',
+    'tr' => 'Turkish',
+    'vi' => 'Vietnamese',
+    'th' => 'Thai',
+    'nl' => 'Dutch',
+    'pl' => 'Polish',
+    'uk' => 'Ukrainian',
+    'sv' => 'Swedish',
+    'no' => 'Norwegian',
+    'fi' => 'Finnish',
+    'da' => 'Danish',
+    'he' => 'Hebrew',
+    'el' => 'Greek',
+    'cs' => 'Czech',
+    'hu' => 'Hungarian',
+    'ro' => 'Romanian',
+    'bg' => 'Bulgarian',
+    'sr' => 'Serbian',
+    'hr' => 'Croatian',
+    'sk' => 'Slovak',
+    'sl' => 'Slovenian',
+    'lt' => 'Lithuanian',
+    'lv' => 'Latvian',
+    'et' => 'Estonian',
+    'is' => 'Icelandic',
+    'mt' => 'Maltese',
+    'ga' => 'Irish',
+    // Boshqa tillar qo'shing
+];
